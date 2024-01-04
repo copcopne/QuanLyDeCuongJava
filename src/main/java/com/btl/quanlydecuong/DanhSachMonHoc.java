e0e4f571e1400e593a2288870eee5632b4a4bac4
Windows10 <Windows10@DESKTOP-9CDUB9J>
2024-01-04 21:33:28 +0700

    hi

--- src/main/java/com/btl/quanlydecuong/DanhSachMonHoc.java
+++ src/main/java/com/btl/quanlydecuong/DanhSachMonHoc.java
@@ -1,7 +1,9 @@
 package com.btl.quanlydecuong;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DanhSachMonHoc {
 
@@ -10,8 +12,20 @@ public class DanhSachMonHoc {
     public DanhSachMonHoc() {
     }
 
-    public void timKiemMon() {
-
+    public DanhSachMonHoc(MonHoc... mh) {
+        this.dsMonHoc.addAll(Arrays.asList(mh));
     }
 
+    public void themMonHoc(MonHoc mh) {
+        dsMonHoc.add(mh);
+    }
+
+    //chua test
+    public List<MonHoc> timKiemMon(String keyword) {
+        List<MonHoc> ds = this.dsMonHoc.stream()
+                .filter(m -> m.getTenMonHoc().equals(keyword) 
+                        || m.getMaMonHoc().equals(keyword))
+                .collect(Collectors.toList());
+        return ds;
+    }
 }
