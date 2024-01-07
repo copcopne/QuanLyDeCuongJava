dcb0a6bab9ce7313da92d850404f48c5a3d3eb72
Windows10 <Windows10@DESKTOP-9CDUB9J>
2024-01-07 14:25:57 +0700

    them ham tao de cuong

--- src/main/java/com/btl/quanlydecuong/DanhSachDeCuong.java
+++ src/main/java/com/btl/quanlydecuong/DanhSachDeCuong.java
@@ -2,7 +2,6 @@ package com.btl.quanlydecuong;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class DanhSachDeCuong {
@@ -23,22 +22,32 @@ public class DanhSachDeCuong {
     public void themDeCuong(DeCuongMonHoc d) {
         this.dsDeCuong.add(d);
     }
-    
+
     public void themDecuong(DeCuongMonHoc... d) {
         this.dsDeCuong.addAll(Arrays.asList(d));
     }
-    
-    public void xoaDeCuong(DeCuongMonHoc d) {
-        this.dsDeCuong.remove(d);
-    }
-    
+
     public DeCuongMonHoc timKiem(String kw) {
-        return this.dsDeCuong.stream().filter(d-> d.getMon().getMaMonHoc()
+        return this.dsDeCuong.stream().filter(d -> d.getMon().getMaMonHoc()
                 .equals(kw)).findFirst().get();
     }
 
+    public void taoDeCuong(MonHoc mon, He heDaoTao, String noiDungMonHoc,
+            String mucTieuMonHoc, String chuanDauRa, GiangVien giangVienBienSoan,
+            DanhSachMonHoc monHocTienQuyet, DanhSachMonHoc monHocTruoc, GiangVien g) {
+        if (g.getSoDeCuongBienSoan() > 5 && ((HeThongQuanLy.isMonDaTonTai(mon) == true 
+                && this.timKiem(mon.getMaMonHoc()).getHeDaoTao().equals(heDaoTao) == false) 
+                || (HeThongQuanLy.isMonDaTonTai(mon) == false))) {
+
+        }
+    }
+
+    public void xoaDeCuong(DeCuongMonHoc d) {
+        this.dsDeCuong.remove(d);
+    }
+
     public void sapXepDeCuong() {
-        
+
     }
 
     public void thongKeDeCuong() {
@@ -47,8 +56,8 @@ public class DanhSachDeCuong {
 
     //chua test
     public void xuatDanhSach() {
-        this.dsDeCuong.forEach(dc->System.out.printf("Giang vien: %s\n%s"
-                ,dc.getGiangVienBienSoan().getTenGiangVien(),dc));
+        this.dsDeCuong.forEach(dc -> System.out.printf("Giang vien: %s\n%s",
+                 dc.getGiangVienBienSoan().getTenGiangVien(), dc));
     }
 
 }
