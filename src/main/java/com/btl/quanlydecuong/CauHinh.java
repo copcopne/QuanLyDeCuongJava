a8820c72905dfb2e5479486fbbe259a4c8c0a2df
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2024-01-08 10:02:53 +0700

    chỉnh sửa

--- src/main/java/com/btl/quanlydecuong/CauHinh.java
+++ src/main/java/com/btl/quanlydecuong/CauHinh.java
@@ -1,7 +1,10 @@
 package com.btl.quanlydecuong;
 
+import java.util.Scanner;
+
 public class CauHinh {
     protected static DeCuongMonHoc DCMonHoc_temp;
+    protected static final Scanner SC = new Scanner(System.in);
 
     public CauHinh() {
     }
