6a775961e9e2943f1a15039da55f087841c09561
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2024-01-10 19:40:48 +0700

    them press enter to continue

--- src/main/java/com/btl/quanlydecuong/CauHinh.java
+++ src/main/java/com/btl/quanlydecuong/CauHinh.java
@@ -1,20 +1,31 @@
 package com.btl.quanlydecuong;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CauHinh {
+
     protected static DeCuongMonHoc DCMonHoc_temp;
     protected static HinhThuc hinhThuc_temp;
     protected static final Scanner SC = new Scanner(System.in);
 
     public CauHinh() {
     }
-    public static boolean CheckInteger(String input) { 
-        try { 
-            Integer.valueOf(input); 
+
+    public static boolean CheckInteger(String input) {
+        try {
+            Integer.valueOf(input);
             return true;
-        } catch (NumberFormatException e) { 
+        } catch (NumberFormatException e) {
             return false;
-        } 
-    } 
+        }
+    }
+
+    public static void pressEnterToContinue() {
+        System.out.println("Press enter to continue...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+        }
+    }
 }
